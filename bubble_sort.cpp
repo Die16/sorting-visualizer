2d2e1bb91dab1283e6969aa7d168e455d12185c9
Diego Guatarasma <134355798+Die16@users.noreply.github.com>
2025-12-04 21:03:29 -0500

    fixed swap functions and created the random vector function to make code clearer. Need to work in implemention of algorithm and logic inside the funtion of bubble sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 std::vector<int> random_vector(int size);
-void swap(int left, int right);
+void swap(int left, int right, std::vector<int> arr);
 void bubble_sort(std::vector<int>);
 void printarr(std::vector<int> arr);
 
@@ -17,9 +17,12 @@ int main() {
   std::vector<int> arr = random_vector(size);
 
   // bubble sorts
+  std::cout << "Unsorted array";
   printarr(arr);
-  std::cout << 1;
-  bubble_sort(arr);
+
+  swap(0, 1, arr);
+
+  std::cout << "sorted array";
   printarr(arr);
 
   return 0;
@@ -28,7 +31,8 @@ int main() {
 void bubble_sort(std::vector<int> arr) {
   for (int i = 0; i < arr.size(); i++)
     for (int j = 1; j < arr.size(); j++) {
-      swap(i, j);
+      if (i > j) {
+      }
     }
 }
 
@@ -39,10 +43,10 @@ void printarr(std::vector<int> arr) {
   std::cout << std::endl;
 }
 
-void swap(int left, int right) {
-  int temp = right;
-  right = left;
-  left = temp;
+void swap(int left, int right, std::vector<int> arr) {
+  int temp = arr[right];
+  arr[right] = arr[left];
+  arr[left] = temp;
 }
 
 std::vector<int> random_vector(int size) {
