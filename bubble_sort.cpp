5bb98396989c9dfb4295b89d45cc61e98b69e811
Diego Guatarasma <134355798+Die16@users.noreply.github.com>
2026-01-07 13:17:48 -0500

    Started from zero to incorporate the <random> library to get better random numbers and understand the program better

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,58 +1,13 @@
-#include <cstdlib> //for rand() fucntion
 #include <iostream>
+#include <random> //for rand() fucntion
 #include <vector>
 
-std::vector<int> random_vector(int size);
-void swap(int left, int right, std::vector<int> arr);
-void bubble_sort(std::vector<int>);
-void printarr(std::vector<int> arr);
+std::vector<int> arrayMaker();
 
 int main() {
-  // variables for the list
-  int size;
-  std::cout << "What size do you want the array?";
-  std::cin >> size;
-
-  // make list of size n with random numbers
-  std::vector<int> arr = random_vector(size);
-
-  // bubble sorts
-  std::cout << "Unsorted array";
-  printarr(arr);
-
-  swap(0, 1, arr);
-
-  std::cout << "sorted array";
-  printarr(arr);
+  std::cout << "hello world";
 
   return 0;
 }
 
-void bubble_sort(std::vector<int> arr) {
-  for (int i = 0; i < arr.size(); i++)
-    for (int j = 1; j < arr.size(); j++) {
-      if (i > j) {
-      }
-    }
-}
-
-void printarr(std::vector<int> arr) {
-  for (int i = 0; i < arr.size(); i++) {
-    std::cout << arr[i] << "  ";
-  }
-  std::cout << std::endl;
-}
-
-void swap(int left, int right, std::vector<int> arr) {
-  int temp = arr[right];
-  arr[right] = arr[left];
-  arr[left] = temp;
-}
-
-std::vector<int> random_vector(int size) {
-  std::vector<int> arr(size);
-  for (int i = 0; i < size; i++) {
-    arr[i] = std::rand() % 50;
-  }
-  return arr;
-}
+std::vector<int> arrayMaker() {}
