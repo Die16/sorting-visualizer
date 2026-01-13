6430c011cfbe87817a0ba3e73c62b2e971852b5a
Diego Guatarasma <134355798+Die16@users.noreply.github.com>
2026-01-13 15:18:22 -0500

    Made the files and operation more modular in orther to make the visualizer easier to implement

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 CXX = g++
 CXXFLAGS = -std=c++17 -Wall -Wextra -O2
 
-bubble_sort: bubble_sort.cpp
-	$(CXX) $(CXXFLAGS) bubble_sort.cpp -o bubble_sort
-
+main: main.cpp
+	$(CXX) $(CXXFLAGS) main.cpp -o main
---
+++ algorithms.cpp
@@ -0,0 +1,16 @@
+#include <random>
+#include <iostream>
+#include <vector>
+ 
+void bubbleSort(std::vector<int> &arr) {
+  int vectorSize = arr.size();
+  for (int i = 0; i < vectorSize - 1; i++) {
+    for (int j = 0; j < vectorSize - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+}
